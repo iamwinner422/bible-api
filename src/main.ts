3dfd19395368bc14f87518a8d91808d02aeb1d97
iamwinner422 <iamwinner422@gmail.com>
2025-10-25 22:13:13 +0000

    chore: setup config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Bible Verses API')
+		.setDescription('This API returns Bible verses')
+		.setVersion('1.0')
+		.addTag('bible-api')
+		.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('/', app, documentFactory);
+
+	app.enableCors();
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
